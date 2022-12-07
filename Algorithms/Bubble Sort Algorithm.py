33c78a727cc10dbd74e68a5a198a63f7c4784925
Oli Bertin <72766787+BertinOG@users.noreply.github.com>
2022-12-07 11:24:23 +0000

    Quicksort + Bubble

--- Algorithms/Bubble Sort Algorithm.py
+++ Algorithms/Bubble Sort Algorithm.py
@@ -1,14 +1,19 @@
-arr = [4,1,8,6,2,12,3,7]
+arr = [4, 1, 8, 6, 2, 12, 3, 7]
 n = len(arr)
 
-for i in range(0, n-2):
-    for j in range(0,(n-i-1)):
-        if arr[j] > arr[j+1]:
-            temp = arr[j]
-            arr[j] = arr[j+1]
-            arr[j+1] = temp
-        # end if
-    #next j
-#next i
 
-print(arr)
+def bubble():
+    swap = False
+    for i in range(0, n - 1):
+        for j in range(0, (n - i - 1)):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            # end if
+        # next j
+    # next i
+    return arr
+
+
+print(bubble())
---
+++ Algorithms/Quicksort Algorithm.py
@@ -0,0 +1,14 @@
+arr = [4, 1, 8, 6, 2, 12, 3, 7]
+
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+
+print(quicksort(arr))
